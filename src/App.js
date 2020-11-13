8a8a063a7940df40b2785c7e43d68dc2827d63d4
Jay Patel <jp9573@gmail.com>
2020-11-13 22:32:28 +0530

    Add instructional comments

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ class App extends Component {
     render() {
         return (
             <div className="container">
+                {/* Pass string as content */}
                 <HighlightMeWrapper
                     content="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat omnis iure assumenda illo alias dicta nostrum aperiam, similique molestias suscipit. Reprehenderit vero animi quaerat veniam modi sed dolor nam omnis."
                 />
+
+                {/* Pass html content as content */}
                 <HighlightMeWrapper>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio, aperiam. Illo inventore, necessitatibus excepturi asperiores, consequatur alias porro, nostrum laborum delectus omnis neque. Tempora est eum deserunt sed totam reiciendis?</p>
                 </HighlightMeWrapper>
